62d2caf5f37ee082530bf69874fb2a03f6f4bb40
Zak Schenck <zakschenck@MacBook-Pro.localdomain>
2023-11-20 19:53:23 -0500

    vue deploy files added

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,2 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/azureleaf-web/' : '/',
-})
+
